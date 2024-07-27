ce5c190b8b44d2c6f0cf6eb74f08d14fe6c46595
victor.verdoodt <victor.verdoodt@becareapp.io>
2024-07-27 12:33:57 -0300

    add: telemetry and ci/cd pipeline

---
+++ FiapTechChallenge.ContactBook.Api/Extensions/Startup/OpenTelemetryExtension.cs
@@ -0,0 +1,58 @@
+﻿using OpenTelemetry.Logs;
+using OpenTelemetry.Metrics;
+using OpenTelemetry.Resources;
+using OpenTelemetry.Trace;
+using Serilog;
+using System.Reflection;
+
+namespace FiapTechChallenge.ContactBook.Presentation.Api.Extensions.Startup
+{
+    public static class OpenTelemetryExtension
+    {
+        public static void AddTelemetry(this IServiceCollection services)
+        {
+            services.AddOpenTelemetry()
+                .ConfigureResource(b =>
+                {
+                    b.AddService(Assembly.GetEntryAssembly().GetName().Name);
+                })
+                .WithLogging(b =>
+                    b.AddOtlpExporter())
+                .WithTracing(b => b
+                    .AddAspNetCoreInstrumentation()
+                    .AddHttpClientInstrumentation()
+                    .AddEntityFrameworkCoreInstrumentation()
+                    .AddOtlpExporter())
+                .WithMetrics(b => b
+                    .AddAspNetCoreInstrumentation()
+                    .AddHttpClientInstrumentation()
+                    .AddRuntimeInstrumentation()
+                    .AddPrometheusExporter());
+
+            services.AddSerilog();
+        }
+
+        public static void UseTelemetrySetup(this IApplicationBuilder app)
+        {
+            app.UseSerilogRequestLogging();
+            app.UseOpenTelemetryPrometheusScrapingEndpoint();
+        }
+
+        public static void AddTelemetryLog(this ILoggingBuilder builder)
+        {
+
+            builder.AddOpenTelemetry(options =>
+            {
+                options.IncludeFormattedMessage = true;
+                options.IncludeScopes = true;
+
+                var resBuilder = ResourceBuilder.CreateDefault();
+                var serviceName = Assembly.GetEntryAssembly().GetName().Name;
+                resBuilder.AddService(serviceName);
+                options.SetResourceBuilder(resBuilder);
+
+                options.AddOtlpExporter();
+            });
+        }
+    }
+}
--- FiapTechChallenge.ContactBook.Api/Program.cs
+++ FiapTechChallenge.ContactBook.Api/Program.cs
@@ -1,5 +1,8 @@
 using FiapTechChallenge.ContactBook.Infrastructure.IOC;
 using FiapTechChallenge.ContactBook.Presentation.Api.Extensions.Startup;
+using Serilog;
+using Serilog.Sinks.Grafana.Loki;
+using System.Reflection;
 
 namespace FiapTechChallenge.ContactBook.Presentation.Api
 {
@@ -8,24 +11,53 @@ namespace FiapTechChallenge.ContactBook.Presentation.Api
         public static void Main(string[] args)
         {
 
-            var builder = WebApplication.CreateBuilder(args);
+            Log.Logger = new LoggerConfiguration()
+#if DEBUG
+                .MinimumLevel.Information()
+#else
+                .MinimumLevel.Error()
+#endif
+                .Enrich.FromLogContext()
+                .WriteTo.Console()
+                .WriteTo.GrafanaLoki("https://loki.rapitify.com",
+                        [new LokiLabel { Key = "service_name", Value = Assembly.GetEntryAssembly().GetName().Name }])
+                .CreateLogger();
+
+            Log.Information("Starting up");
+
+            try
+            {
+                var builder = WebApplication.CreateBuilder(args);
 
             IConfiguration configuration = builder.Configuration;
 
+            
             builder.AddServiceDefaults();
             builder.AddDatabaseSetup();
             builder.AddRedisSetup();
+            builder.Services.AddTelemetry();
             builder.Services.AddAutoMapperSetup();
             builder.Services.AddSwaggerSetup();
             builder.Services.AddDefaultSetup();
             builder.Services.AddHealthSetup(configuration);
             RegisterServices(builder.Services, configuration);
+
             var app = builder.Build();
 
+            app.UseTelemetrySetup();
             app.UseSwaggerSetup();
             app.UseDefaultSetup();
 
             app.Run();
+            }
+            catch (Exception ex)
+            {
+                Log.Fatal(ex, "Unhandled exception");
+            }
+            finally
+            {
+                Log.CloseAndFlush();
+            }
         }
 
         private static void RegisterServices(IServiceCollection services, IConfiguration configuration)
