309cac1ac571b44f91add88c89d5a1fa258e775a
victor.verdoodt <victor.verdoodt@becareapp.io>
2024-12-01 14:29:16 -0300

    Update version

--- FiapTechChallenge.ContactBook.AppHost/Program.cs
+++ FiapTechChallenge.ContactBook.AppHost/Program.cs
@@ -10,13 +10,17 @@ var rabbitMq = builder.AddRabbitMQ("RabbitMQConnection", port: 5672);
 var api = builder.AddProject<Projects.FiapTechChallenge_ContactBook_Presentation_Api>("api")
     .WithReference(rabbitMq)
     .WithReference(database)
-    .WithReference(cache);
+    .WithReference(cache)
+    .WaitFor(database)
+    .WithReplicas(2);
 
 builder.AddProject<Projects.FiapTechChallenge_ContactBook_Presentation_WebApp>("webapp")
     .WithReference(rabbitMq)
     .WithReference(api)
+    .WaitFor(api)
     .WithExternalHttpEndpoints();
 
-builder.AddProject<Projects.FiapTechChallenge_ContactBook_Presentation_Consumer>("consumer");
+builder.AddProject<Projects.FiapTechChallenge_ContactBook_Presentation_Consumer>("consumer")
+    .WaitFor(rabbitMq);
 
 builder.Build().Run();
